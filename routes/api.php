4e02dee4c5d80a6ab71260fc1a158710019077d6
Emanuel <manupalacios19@gmail.com>
2020-09-12 17:24:38 -0300

    Add Login

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('articles', 'ArticleController');
 
 Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
